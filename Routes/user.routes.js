5cf186f0dd12501007cc9278490870895a5d4438
achury2000 <achurycristianmateo@gmail.com>
2025-07-07 13:32:13 -0500

    desplegar-api

---
+++ Routes/user.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../Controllers/user.controller');
+
+// Obtener todos los usuarios
+router.get('/', getUsers);
+
+// Crear un nuevo usuario
+router.post('/', createUser);
+
+// Actualizar un usuario por ID
+router.put('/:id', updateUser);
+
+// Eliminar un usuario por ID
+router.delete('/:id', deleteUser);
+
+module.exports = router;
